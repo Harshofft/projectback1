3ad1b011a01b0930703f736058a8a3ff6377c705
Harshofft <hdharsh122@gmail.com>
2025-03-06 10:35:19 +0530

    update small changes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express'; 
-import cors from 'cors'          
+import cors from 'cors'   
+import dotenv from 'dotenv'
+dotenv.config();       
 const app = express();
 
 app.use(express.json());    
 app.use(cors())
-const PORT = 5001;
+const PORT = process.env.PORT;
 
 
 let FOODITEM =[
